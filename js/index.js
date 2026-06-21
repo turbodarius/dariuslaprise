c9f5d1f975e7e74779fa915c5075c0f29312d696
turbodarius <139899471+turbodarius@users.noreply.github.com>
2026-06-21 15:49:15 -0400

    Added js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+
+function generateRandomColor()
+{
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+    //random color will be freshly served
+}
